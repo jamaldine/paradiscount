e0bd996e025ad8f8058b6b5098098af9abf75408
jamaldine <jboukir@gmail.com>
2021-05-02 01:35:23 +0000

    test with akram

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,7 @@
     <div>
         <!--<img src="{{asset('/images/nuxe.jpeg')}}" alt="nuxe" />-->
     </div>
-    <h1>Hello Akram, how are you doing my friend? I'm jamaldine :)</h1>
+    <h1>Hello Akram, how are you doing my friend? I'm jamaldine... :)</h1>
     
 </div>
 @endsection
